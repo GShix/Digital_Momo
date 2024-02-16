590cb14c1728e9a4ee6c6796bbf5a753b425d585
Dambar Sing <programmer.gshi18@gmail.com>
2024-02-17 00:35:07 +0545

    Verify the entered OTP with userFound[0].otp

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
-const { registerUser, loginUser, forgotPassword } = require('../controllers/auth/authControllers');
+const { registerUser, loginUser, forgotPassword, verifyOtp } = require('../controllers/auth/authControllers');
 
 const router = require('express').Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/forgotpassword").post(forgotPassword);
+router.route("/verifyotp").post(verifyOtp);
 
 module.exports =router
