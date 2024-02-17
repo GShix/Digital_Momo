d53d33ceb65b604858198ee7cce9f34f7a0c5371
Dambar Sing <programmer.gshi18@gmail.com>
2024-02-17 22:04:42 +0545

    Admin data seeding

--- database/database.js
+++ database/database.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
+const User = require('../model/userModel');
 exports.connectDatabase = async(URI)=>{
     await mongoose.connect(URI)
     console.log("Database is connected successfully");
+    const adminFound = await User.findOne({userEmail:"admin11@gmail.com"})
+    if(!adminFound){
+        await User.create({
+            userEmail:"admin11@gmail.com",
+            userPhoneNumber:"943848744",
+            userPassword:"Admin",
+            userName:"admin",
+            role:"admin"
+        })
+        console.log("Admin seeded successfully")
+    }else{
+        console.log("Admin already exists")
+    }
 }
+
