dbd996655710ea2496d15576a9c17e56a2128009
Dambar Sing <programmer.gshi18@gmail.com>
2024-02-26 00:55:40 +0545

    userPassword updated

--- controllers/user/profile/profileController.js
+++ controllers/user/profile/profileController.js
@@ -1,5 +1,5 @@
 const User = require("../../../model/userModel")
-
+const bcrypt = require('bcryptjs')
 
 //get my profile
 exports.getMyProfile= async(req,res)=>{
@@ -30,5 +30,34 @@ exports.deleteProfile = async(req,res)=>{
     res.status(200).json({
         message:"Profile deleted",
         data:null
+    })
+}
+
+//update password
+exports.updatePassword = async(req,res)=>{
+    const userId = req.user.id
+    const {oldPassword, confirmPassword, newPassword} = req.body
+    if(!oldPassword || !confirmPassword || !newPassword){
+        return res.status(200).json({
+            message:"Enter your old Password, newPassword & confirmPassword"
+        })
+    }
+    if(newPassword !== confirmPassword){
+        return res.status(200).json({
+            message:"Your password doesn't match"
+        })
+    }
+    const userData = await User.findById(userId)
+    const hashedOldPass = userData.userPassword
+    const isOldPasswordCorrect = bcrypt.compareSync(oldPassword,hashedOldPass)
+    if(!isOldPasswordCorrect){
+        return res.status(400).json({
+            message:"You don't have permission"
+        })
+    }
+    userData.userPassword = bcrypt.hashSync(newPassword,12)
+    await userData.save()
+    res.status(200).json({
+        message:"Password Updated"
     })
 }
