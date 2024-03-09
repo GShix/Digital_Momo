61d6042acb2508b621674d002f0a937b5cdebb8a
Dambar Sing <programmer.gshi18@gmail.com>
2024-03-10 00:05:21 +0545

    Navbar from tailwind

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -1,3 +1,5 @@
+import Navbar from "../../globals/components/navbar/Navbar"
+
 const products = [
     {
       id: 1,
@@ -13,6 +15,8 @@ const products = [
   
   export default function Home() {
     return (
+    <>    
+    <Navbar/>
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
@@ -44,5 +48,6 @@ const products = [
           </div>
         </div>
       </div>
+    </>
     )
   }
