a5ac3a178f2bc5d651ad34f71569e43975f59fe7
Dambar Sing <programmer.gshi18@gmail.com>
2024-04-17 22:52:18 +0545

    ProductDetails

--- Frontend/src/pages/productDetail/component/product/ProductDeta.jsx
+++ Frontend/src/pages/productDetail/component/product/ProductDeta.jsx
@@ -29,7 +29,7 @@ const ProductDeta = () => {
             </svg>
             <span className="text-gray-600 ml-3">4 Reviews</span>
           </span>
-          <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
+          <span className="flex ml-3 pl-3 py-2i border-l-2 border-gray-200">
             <a className="text-gray-500">
               <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
