ebb3c6e1e26ed56e8a111ee950a856c1f5a33fcd
Dambar Sing <programmer.gshi18@gmail.com>
2024-04-17 12:30:38 +0545

    loginUser() and token state

--- Frontend/src/pages/auth/login/Login.jsx
+++ Frontend/src/pages/auth/login/Login.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-import Form from '../../../globals/components/forms/Form'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUser } from '../../../store/authSlice';
+import { STATUSES } from '../../../globals/misc/Statuses';
 
 const Login = () => {
-  const Navigate = useNavigate()
+  const Navigate = useNavigate();
+  const dispatch = useDispatch();
+  const {data,status} = useSelector((state)=>state.auth);
+  const [userData,setUserData] =useState({
+    email:"",
+    password:""
+  })
+  const handleChange =(e)=>{
+    const {name,value}=e.target;
+    setUserData({
+      ...userData,
+      [name]:value
+    })
+  }
+  console.log(userData)
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    dispatch(loginUser(userData))
+    if(status == STATUSES.SUCCESS){
+      return Navigate("/")
+    }
+    if(status == STATUSES.ERROR){
+      alert("Something went wrong, Please Try Again")
+    }
+  }
   return (
     <div>
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -14,12 +40,13 @@ const Login = () => {
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Hello! <strong>{data.userName}</strong><br></br>
             Login to your account
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
@@ -28,7 +55,7 @@ const Login = () => {
                 <input
                   id="email"
                   name="email"
-                  type="email"
+                  type="email" onChange={handleChange}
                   autoComplete="email"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -51,7 +78,7 @@ const Login = () => {
                 <input
                   id="password"
                   name="password"
-                  type="password"
+                  type="password" onChange={handleChange}
                   autoComplete="current-password"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -71,7 +98,7 @@ const Login = () => {
 
           <p className="mt-10 text-center text-sm text-gray-500">
             Not a member?{' '}
-            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+            <a onClick={()=>Navigate('/register')} href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
               Start a 14 day free trial
             </a>
           </p>
--- Frontend/src/store/authSlice.js
+++ Frontend/src/store/authSlice.js
@@ -6,7 +6,8 @@ const authSlice = createSlice({
     name:'product',
     initialState:{
         data:[],
-        status: STATUSES.SUCCESS
+        status: STATUSES.SUCCESS,
+        token:""
     },
     reducers:{
         setUser(state,action){
@@ -14,19 +15,35 @@ const authSlice = createSlice({
         },
         setStatus(state,action){
             state.status = action.payload
+        },
+        setToken(state,action){
+            state.token = action.payload
         }
     }
 })
-export const {setUser,setStatus} = authSlice.actions
+export const {setUser,setStatus,setToken} = authSlice.actions
 export default authSlice.reducer
 
 
 export function registerUser(data){
+    return async function registerUserThunk(dispatch,getState){
+        dispatch(setStatus(STATUSES.LOADING))
+        try {
+            const response = await  axios.post("http://localhost:4000/api/auth/register",data);
+            dispatch(setUser(response.data.data))
+            dispatch(setStatus(STATUSES.SUCCESS))
+        } catch (error) {
+            console.log(error);
+            dispatch(setStatus(STATUSES.ERROR))
+        }
+    }
+}
+export function loginUser(data){
     return async function loginUserThunk(dispatch,getState){
         dispatch(setStatus(STATUSES.LOADING))
         try {
-            const response = await  axios.post("http://localhost:40t00/api/auth/registerr",data);
-            dispatch(setUser(response.data.data))
+            const response = await  axios.post("http://localhost:4000/api/auth/login",data);
+            dispatch(setToken(response.data.data))
             dispatch(setStatus(STATUSES.SUCCESS))
         } catch (error) {
             console.log(error);
